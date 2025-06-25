cdfc09c4c16c4bfc28beb83b2f4995ee7409b7c4
chao@torii.co <chao@torii.co>
2025-06-25 14:00:52 -0400

    Add ControlsProvider as part of the exporting options

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 // Core exports
 export { default as Playground } from "@/components/Playground";
-export { useControls, useUrlSyncedControls } from "@/context/ControlsContext";
+export {
+  useControls,
+  useUrlSyncedControls,
+  ControlsProvider,
+} from "@/context/ControlsContext";
 
 // Type exports
 export type { ControlsSchema, ControlType } from "@/context/ControlsContext";
